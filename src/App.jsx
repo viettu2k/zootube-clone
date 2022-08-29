67c96eba623d0c0162e527c47edf5cea73446878
viettu2k <doanviettu2k@gmail.com>
2022-08-29 14:39:35 +0700

    folder sturce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000" }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" exact element={<VideoDetail />} />
+            <Route path="/channel/:id" exact element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
